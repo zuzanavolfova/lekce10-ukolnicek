3c790cfa82147499ab034a2c963f9ada7d90b562
karolyim <matej.karolyi@gmail.com>
2021-05-04 15:55:37 +0200

    [MAINTENANCE] project init

---
+++ main.js
@@ -0,0 +1,62 @@
+// do proměnné si uložíme prvek, do kterého budeme vypisovat seznam [úkol]
+
+// založíme si prázdné pole, ve kterém budeme uchovávat úkoly
+
+// v poli budou úkoly jako objekty, každý úkol má popis a důležitost
+// když něco v poli bude, mělo by to vypadat takto:
+/*
+ukoly = [
+	{
+		popis: 'Koupit chleba',
+		dulezitost: 'vysoká'
+	},
+	{
+		popis: 'Vyvenčit psa',
+		dulezitost: 'střední'
+	},
+	... atd.
+];
+*/
+
+// ihned po načtení stránky zavoláme funkci,
+// která přečte seznam úkolů z Local Storage
+
+// a hned seznam úkolů zobrazíme
+
+
+
+// funkce pro načtení seznamu úkolů z Local Storage
+function nactiUkoly() {
+
+}
+
+
+// funkce pro uložení seznamu úkolů do Local Storage
+function ulozUkoly() {
+
+}
+
+
+// funkce pro zobrazení seznamu úkolů na stránce
+function zobrazUkoly() {
+
+}
+
+
+// funkce, která vytvoří HTML prvky jednoho úkolu
+// jako parametry očekává index úkolu v poli, popis a důležitost úkolu
+function vytvorPrvekUkolu(index, popis, dulezitost) {
+
+}
+
+
+// funkce pro smazání úúkolu při kliknutí na tlačítko "x" vedle popisu úkolu
+function odstranUkol() {
+
+}
+
+
+// funkce pro přidání úkolu do seznamu
+function pridejUkol() {
+
+}
