b480cd5153773e605a0449732d699491eb67113c
Zuzana Volfova <zuzka.volfova@gmail.com>
2022-04-21 20:38:29 +0200

    add date

--- main.js
+++ main.js
@@ -50,7 +50,7 @@ function zobrazUkoly() {
 seznam.innerHTML = '';
 if (ukoly.length > 0) {
 	for (let i =0; i<ukoly.length; i++ ) {
-		let ukol = vytvorPrvekUkolu(i, ukoly[i].popis, ukoly[i].dulezitost);
+		let ukol = vytvorPrvekUkolu(i, ukoly[i].popis, ukoly[i].dulezitost, ukoly[i].datum);
 		seznam.appendChild(ukol);
 	}
 }
@@ -59,9 +59,9 @@ if (ukoly.length > 0) {
 
 // funkce, která vytvoří HTML prvky jednoho úkolu
 // jako parametry očekává index úkolu v poli, popis a důležitost úkolu
-function vytvorPrvekUkolu(index, popis, dulezitost) {
+function vytvorPrvekUkolu(index, popis, dulezitost, datum) {
 	let liElement = document.createElement('li');
-	liElement.textContent = popis + ' - ' + dulezitost + " důležitost";
+	liElement.textContent = popis + ' - ' + dulezitost + " důležitost " + datum;
 
 	let buttonElement= document.createElement('button');
 	buttonElement.textContent='x';
@@ -87,6 +87,7 @@ function odstranUkol() {
 function pridejUkol() {
 	let popis = document.querySelector('#popis').value;
 	let dulezitost = document.querySelector('#dulezitost').value;
+	let datum =  document.querySelector('#date').value;
 	if (popis=== '') {
 		alert ('Prosim, zadej popis ukolu.');
 		return;
@@ -95,6 +96,7 @@ function pridejUkol() {
 	let ukol = {};
 	ukol.popis = popis;
 	ukol.dulezitost = dulezitost;
+	ukol.datum = datum;
 
 	ukoly.push(ukol);
 
